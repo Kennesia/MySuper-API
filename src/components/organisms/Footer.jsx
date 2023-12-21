bf152912a2f5b28954a5393f78aec8761e1465be
MyKennesia <154445838+Kennesia@users.noreply.github.com>
2023-12-21 21:47:54 +0700

    Update Footer.jsx

--- src/components/organisms/Footer.jsx
+++ src/components/organisms/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
-            <Logo href="/" src="/megane.png" name="Megane API" />
+            <Logo href="/" src="/megane.png" name="MySuper API" />
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
             <div>
@@ -34,7 +34,7 @@ export default function Footer() {
           <span className="text-sm text-dark sm:text-center dark:text-subdark">
             © {new Date().getFullYear() + " "}
             <LinkItem href="/" className="hover:underline">
-              Megane API
+              MySuper API
             </LinkItem>
             . All Rights Reserved.
           </span>
